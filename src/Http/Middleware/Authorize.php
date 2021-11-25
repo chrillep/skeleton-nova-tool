6e711b1d5b7e49225125db030e9560032a1fdf28
Christian Widlund <christian.widlund@gmail.com>
2021-11-25 20:39:52 +0100

    Replace all (#8)
    * Replace all
    
    * amespace_vendor -> namespace_vendor
    
    * Apply fixes from StyleCI (#9)
    
    Co-authored-by: Christian Widlund <chrillep@users.noreply.github.com>
    
    Co-authored-by: Christian Widlund <chrillep@users.noreply.github.com>

--- src/Http/Middleware/Authorize.php
+++ src/Http/Middleware/Authorize.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace :namespace_vendor\:namespace_tool_name\Http\Middleware;
+namespace namespace_vendor\namespace_tool_name\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use :namespace_vendor\:namespace_tool_name\Tool;
+use namespace_vendor\namespace_tool_name\Tool;
 use Symfony\Component\HttpFoundation\Response;
 
 class Authorize
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace :namespace_vendor\:namespace_tool_name\Tests;
+namespace namespace_vendor\namespace_tool_name\Tests;
 
 use Illuminate\Support\Facades\Route;
+use namespace_vendor\namespace_tool_name\ToolServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
-use :namespace_vendor\:namespace_tool_name\ToolServiceProvider;
 
 abstract class TestCase extends Orchestra
 {
