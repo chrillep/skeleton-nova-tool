2bfda4c7b65b281d9f4d43ef1b7dd1dd503c09f3
Christian Widlund <chrillep@users.noreply.github.com>
2021-11-25 19:34:20 +0000

    Apply fixes from StyleCI

--- tests/ToolControllerTest.php
+++ tests/ToolControllerTest.php
@@ -2,10 +2,6 @@
 
 namespace namespace_vendor\namespace_tool_name\Tests;
 
-use namespace_vendor\namespace_tool_name\Http\Controllers\ToolController;
-use namespace_vendor\namespace_tool_name\namespace_tool_name;
-use Symfony\Component\HttpFoundation\Response;
-
 class ToolControllerTest extends TestCase
 {
     /** @test */
